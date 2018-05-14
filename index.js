5d0a803ec808e5cce072979f45956b266aaea099
John <crawfo58@purdue.edu>
2018-05-14 14:17:20 -0400

    Button changes header text

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('It works!')
+const changeText = function() {
+    document.getElementsByClassName('header1')[0].innerHTML = 'Button Clicked'
+  }
+const button = document.querySelector('button')
+button.addEventListener('click', changeText)
