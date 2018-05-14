f882113b5b08107936d51ab74dc1b86436f81e2f
John <crawfo58@purdue.edu>
2018-05-14 14:23:04 -0400

    Button changes second header text

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('It works!')
 const changeText = function() {
-    document.getElementsByClassName('header1')[0].innerHTML = 'Button Clicked'
+    document.getElementsByClassName('header2')[0].innerHTML = 'Button Clicked'
   }
 const button = document.querySelector('button')
 button.addEventListener('click', changeText)
