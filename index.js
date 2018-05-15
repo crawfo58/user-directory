937dd007f0025ce372db64632ab03aca166c2ff9
John <crawfo58@purdue.edu>
2018-05-15 11:00:46 -0400

    Add age, reset form each time

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const changeText = function(ev) {
     ev.preventDefault()
     const users = document.querySelector('#users')
     const userName = form.userName.value
-    users.innerHTML += '<p>' + userName + '</p>'
-    form.userName.value = ''
+    const age = form.age.value
+    users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
+    form.reset()
 }
 const form = document.querySelector('form#userForm')
 form.addEventListener('submit', changeText)
