ddf288a502d1fea884bbe9c3ffcf4b54cdd8897c
John <crawfo58@purdue.edu>
2018-05-15 13:37:02 -0400

    Display color in an empty div

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-console.log('It works!')
-const changeText = function(ev) {
-    ev.preventDefault()
-    const users = document.querySelector('#users')
-    const userName = form.userName.value
-    const age = form.age.value
-    const color = form.favoriteColor.value
-
-    const list = document.createElement('ul')
-
-    const nameItem = document.createElement('li')
-    nameItem.textContent = `Name: ${userName}`
-
-    const ageItem = document.createElement('li')
-    ageItem.textContent = `Age: ${age}`
-
-    list.appendChild(nameItem)
-    list.appendChild(ageItem)
-
-    users.appendChild(list)
-
-    form.reset()
-    form.userName.focus()
-}
 const form = document.querySelector('form#userForm')
-form.addEventListener('submit', changeText)
 
+const changeText = function(ev) {
+  ev.preventDefault()
+  const form = ev.target
+  const userName = form.userName.value
+  const age = form.age.value
+  const favoriteColor = form.favoriteColor.value
+
+  const users = document.querySelector('#users')
+
+  const list = document.createElement('ul')
+
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${userName}`
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+
+  const colorItem = document.createElement('li')
+  colorItem.textContent = 'Favorite Color: '
+
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = favoriteColor
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  colorItem.appendChild(colorDiv)
+
+  list.appendChild(nameItem)
+  list.appendChild(ageItem)
+  list.appendChild(colorItem)
+
+  users.appendChild(list)
+
+  form.reset()
+  form.userName.focus()
+}
+
+form.addEventListener('submit', changeText)
