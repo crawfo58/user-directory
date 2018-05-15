07122ace716c3f27468c1a5fdb7b7c2bf062ec91
John <crawfo58@purdue.edu>
2018-05-15 10:44:35 -0400

    Add multiple names, clear input of box each time, autofocus cursor to box

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log('It works!')
 const changeText = function(ev) {
     ev.preventDefault()
-    const heading = document.querySelector('h1')
-    const input = document.querySelector('input')
-    heading.textContent = input.value
+    const users = document.querySelector('#users')
+    const userName = form.userName.value
+    users.textContent += ' ' + userName
+    form.userName.value = ''
 }
 const form = document.querySelector('form#userForm')
 form.addEventListener('submit', changeText)
