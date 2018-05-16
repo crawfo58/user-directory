5efd094e71a5a26e8351b416b68c24dd085296ec
John <crawfo58@purdue.edu>
2018-05-16 09:50:03 -0400

    Streamline renderColor and renderListItem

--- index.js
+++ index.js
@@ -10,40 +10,36 @@ const changeText = function(ev) {
   form.userName.focus()
 }
 
-const renderColor = function() {
-  const favoriteColor = form.favoriteColor.value
+const renderColor = function(color) {
   const colorDiv = document.createElement('div')
-  colorDiv.style.backgroundColor = favoriteColor
+  colorDiv.style.backgroundColor = color
   colorDiv.style.width = '6rem'
   colorDiv.style.height = '3rem'
   return colorDiv
 }
 
-const renderListItem = function() {
-    return document.createElement('li')
+const renderListItem = function(label, value) {
+    const item = document.createElement('li')
+    item.textContent = `${label}: ${value}`
+    return item
 }
 
 const renderList = function() {
     const userName = form.userName.value
     const age = form.age.value
+    const favoriteColor = form.favoriteColor.value
     
     const users = document.querySelector('#users')
 
     const list = document.createElement('ul')
-
-    const nameItem = renderListItem()
-    nameItem.textContent = `Name: ${userName}`
-  
-    const ageItem = renderListItem()
-    ageItem.textContent = `Age: ${age}`
   
     const colorItem = renderListItem()
     colorItem.textContent = 'Favorite Color: '
   
-    colorItem.appendChild(renderColor())
+    colorItem.appendChild(renderColor(favoriteColor))
   
-    list.appendChild(nameItem)
-    list.appendChild(ageItem)
+    list.appendChild(renderListItem('Name', userName))
+    list.appendChild(renderListItem('Age', age))
     list.appendChild(colorItem)
 
     return list
